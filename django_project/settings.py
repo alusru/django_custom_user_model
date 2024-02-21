24010d05da3494611164e9cf7f84cd32d3755dcc
alusru <collen.nkabinde@gmail.com>
2024-02-21 20:08:58 +0200

    added style on login and resgister forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
@@ -126,4 +128,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home" 
-LOGOUT_REDIRECT_URL = "home" 
+LOGOUT_REDIRECT_URL = "home"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
